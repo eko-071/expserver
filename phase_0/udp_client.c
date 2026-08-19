1433d12146f8691655269b9302ba58c2b8a2f6d2
eko-071 <fahad_ali.07@proton.me>
2026-08-19 09:22:44 +0530

    Completed Stage 3

---
+++ phase_0/udp_client.c
@@ -0,0 +1,42 @@
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#define SERVER_PORT 8000
+#define SERVER_ADDR "127.0.0.1"
+#define BUFF_SIZE 10000
+
+int main(){
+    char buffer[BUFF_SIZE];
+    char message[BUFF_SIZE];
+
+    int client_sock_fd = socket(AF_INET, SOCK_DGRAM, 0);
+    
+    struct sockaddr_in server_addr;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = inet_addr(SERVER_ADDR);
+    server_addr.sin_port = htons(SERVER_PORT);
+
+    while (1){
+        printf("Enter a string: ");
+        fgets(message, BUFF_SIZE, stdin);
+
+        // Sending the message to server
+        sendto(client_sock_fd, message, strlen(message), 0, (struct sockaddr*)&server_addr, sizeof(server_addr));
+
+        // Receiving the message from server now
+        ssize_t n = recvfrom(client_sock_fd, buffer, BUFF_SIZE, 0, NULL, NULL);
+        buffer[n] = '\0';
+
+        printf("[SERVER MESSAGE] %s", buffer);
+    }
+
+    // Closing the socket
+    close(client_sock_fd);
+    return 0;
+}
---
+++ phase_0/udp_server.c
@@ -0,0 +1,74 @@
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <pthread.h>
+
+#define PORT 8000
+#define BUFF_SIZE 10000
+
+typedef struct{
+    char message[BUFF_SIZE];
+    struct sockaddr_in client_addr;
+    int sockfd;
+    socklen_t addr_len;
+} client_data_t;
+
+void string_reverse(char *str) {
+    for (int start=0, end=strlen(str)-2; start<end; start++, end--) {
+        char temp = str[start];
+        str[start] = str[end];
+        str[end] = temp; 
+    }
+}
+
+void* handle_client(void* arg){
+    client_data_t* data = (client_data_t*)arg;
+    printf("[CLIENT MESSAGE] %s", data->message);
+    string_reverse(data->message);
+
+    sendto(data->sockfd, data->message, strlen(data->message), 0, (struct sockaddr*)&(data->client_addr), data->addr_len);
+
+    free(data);
+    pthread_exit(NULL);
+}
+
+int main(){
+    char buffer[BUFF_SIZE];
+    pthread_t thread_id;
+    int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    socklen_t len;
+    
+    struct sockaddr_in serv_addr, client_addr;
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv_addr.sin_port = htons(PORT);
+
+    bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+
+    printf("[INFO] Server ready on port %d\n", PORT);
+
+    while(1){
+        len = sizeof(client_addr);
+        ssize_t n = recvfrom(sockfd, buffer, BUFF_SIZE, 0,(struct sockaddr*)&client_addr, &len);
+        buffer[n] = '\0';
+
+        client_data_t* data = (client_data_t*)malloc(sizeof(client_data_t));
+        strcpy(data->message, buffer);
+        data->client_addr = client_addr;
+        data->sockfd = sockfd;
+        data->addr_len = len;
+
+        if (pthread_create(&thread_id, NULL, handle_client, (void*)data) != 0){
+            perror("Failed to create thread");
+            free(data);
+        }
+        else pthread_detach(thread_id);
+    }
+
+    close(sockfd);
+}
